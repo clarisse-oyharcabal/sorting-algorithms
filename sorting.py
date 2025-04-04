b245460093d3960eb88602cfb52c3d29d9826888
clarisse-oyharcabal <clarisse.oyharcabal@laplateforme.io>
2025-04-04 13:39:13 +0200

    <feat>: add quick algorithm method

--- sorting.py
+++ sorting.py
@@ -1,3 +1,16 @@
-class SortingAlgorithm : 
-    def __init__ (self, arr) : 
-        self.arr = arr 
+class SortingAlgorithm:
+    def __init__(self, data):
+        self.data = data
+
+    def quick_sort(self, array=None):
+        if array is None:
+            array = self.data
+
+        if not array:
+            return []
+
+        pivot = array[-1]
+        less = [x for x in array if x < pivot]
+        greater_or_equal = [x for x in array[:-1] if x >= pivot]
+
+        return self.quick_sort(less) + [pivot] + self.quick_sort(greater_or_equal)
