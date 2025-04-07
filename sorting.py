3cdbb6823311694fa573be3811457d9a80e1ca1d
clarisse-oyharcabal <clarisse.oyharcabal@laplateforme.io>
2025-04-07 17:57:17 +0200

    <feat>: create selection sort method

--- sorting.py
+++ sorting.py
@@ -1,3 +1,15 @@
 class SortingAlgorithm: 
     def __init__ (self, arr) : 
         self.arr = arr 
+
+    def selection_sort(self):
+        arr = self.arr  # On travaille directement sur l’attribut
+
+        for i in range(len(arr)):
+            min_idx = i
+            for j in range(i + 1, len(arr)):
+                if arr[j] < arr[min_idx]:
+                    min_idx = j
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+        return arr  # Optionnel : retourne la liste triée
